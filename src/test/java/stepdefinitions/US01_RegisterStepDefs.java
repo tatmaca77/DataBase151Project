a4cb6a6ba259add4f67bf64168a1a52d59728128
yasinaniltechpro <yasinanil.techproed@gmail.com>
2023-08-31 18:37:42 +0300

    2

--- src/test/java/stepdefinitions/US01_RegisterStepDefs.java
+++ src/test/java/stepdefinitions/US01_RegisterStepDefs.java
@@ -1,18 +1,29 @@
 package stepdefinitions;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.HomePage;
 import pages.RegisterPage;
 import utilities.Driver;
+import utilities.JDBCUtils;
 import utilities.ReusableMethods;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static utilities.JSUtils.clickElementByJS;
 
 public class US01_RegisterStepDefs {
     RegisterPage registerPage = new RegisterPage();
     HomePage homePage = new HomePage();
+    private static String fakeUsername;
+    private static String fakeSsn;
+    private static String fakePhoneNumber;
+    ResultSet resultSet;
 
 
     @Given("go to the {string}")
@@ -33,14 +44,25 @@ public class US01_RegisterStepDefs {
 
     @When("Enter name {string}, surname {string},birth_place {string}, phone_number {string},gender {string}, birth_day {string},ssn {string},username {string},password{string}")
     public void enterNameSurnameBirth_placePhone_numberGenderBirth_daySsnUsernamePassword(String name, String surname, String birth_place, String phone_number, String gender, String birth_day, String ssn, String username, String password) {
+        Faker faker = new Faker();
         registerPage.nameInput.sendKeys(name);
         registerPage.surNameInput.sendKeys(surname);
         registerPage.birthPlaceInput.sendKeys(birth_place);
-        registerPage.phoneNumberInput.sendKeys(phone_number);
-        clickElementByJS(registerPage.femaleRadioButton);
+        fakePhoneNumber = faker.number().numberBetween(100, 999) + "-" + faker.number().numberBetween(100, 999) + "-" + faker.number().numberBetween(1000, 9999);
+        registerPage.phoneNumberInput.sendKeys(fakePhoneNumber);
+
+        if (gender.equalsIgnoreCase("male")){
+            clickElementByJS(registerPage.maleRadioButton);
+        }else {
+            clickElementByJS(registerPage.femaleRadioButton);
+        }
+
+
         registerPage.birthDay.sendKeys(birth_day);
-        registerPage.ssnInput.sendKeys(ssn);
-        registerPage.userNameInput.sendKeys(username);
+        fakeSsn = faker.idNumber().ssnValid();
+        registerPage.ssnInput.sendKeys(fakeSsn);
+        fakeUsername = faker.name().username();
+        registerPage.userNameInput.sendKeys(fakeUsername);
         registerPage.passwordInput.sendKeys(password);
     }
 
@@ -50,7 +72,7 @@ public class US01_RegisterStepDefs {
         Thread.sleep(3000);
         RegisterPage registerPage = new RegisterPage();
         clickElementByJS(registerPage.registerButton);
-        ReusableMethods.waitForVisibility(registerPage.alertMessage,5);
+        ReusableMethods.waitForVisibility(registerPage.alertMessage, 5);
         assertTrue(registerPage.alertMessage.getText().contains("Guest User registered"));
 
     }
@@ -61,5 +83,44 @@ public class US01_RegisterStepDefs {
 
         Driver.getDriver().quit();
 
+    }
+
+    @When("connect to database")
+    public void connectToDatabase() {
+
+
+    }
+
+
+    @Given("get guest user via username {string}")
+    public void getGuestUserViaUsername(String username) throws SQLException {
+
+        String sqlQuery = "SELECT * FROM guest_user WHERE username ='" + fakeUsername + "'";
+
+        resultSet = JDBCUtils.executeQuery(sqlQuery);
+        resultSet.next();
+
+
+    }
+
+    @Then("body contains birth_day {string}, birth_place {string}, gender {string}, name {string}, phone_number {string}, ssn {string}, surname {string}, username {string}")
+    public void body_contains_birth_day_birth_place_gender_name_phone_number_ssn_surname_username(String birth_day, String birth_place, String gender, String name, String phone_number, String ssn, String surname, String username) throws SQLException {
+
+        String actBirth_day = resultSet.getString("birth_day");
+        String actBirth_place = resultSet.getString("birth_place");
+        String actGender = resultSet.getString("gender");
+        String actName = resultSet.getString("name");
+        String actPhone_number = resultSet.getString("phone_number");
+        String actSsn = resultSet.getString("ssn");
+        String actSurname = resultSet.getString("surname");
+        String actUsername = resultSet.getString("username");
+        assertEquals(birth_day, actBirth_day);
+        assertEquals(birth_place.trim(), actBirth_place.trim());
+        assertEquals(gender, actGender);
+        assertEquals(name, actName);
+        assertEquals(fakePhoneNumber, actPhone_number);
+        assertEquals(fakeSsn, actSsn);
+        assertEquals(surname.trim(), actSurname.trim());
+        assertEquals(fakeUsername, actUsername);
     }
 }
--- src/test/resources/features/US01_Register.feature
+++ src/test/resources/features/US01_Register.feature
@@ -11,3 +11,13 @@ Feature: Registration Feature
     Examples:
       | username  | birth_day  | birth_place | gender | name | phone_number | ssn         | surname | password |
       | johndoe13 | 01-01-2000 | New York    | Male   | John | 332-654-3987 | 139-35-7836 | Doe     | John.123 |
+
+
+  Scenario Outline: TC_02_GuestUser
+    When connect to database
+    Given get guest user via username "<username>"
+    Then body contains birth_day "<birth_day>", birth_place "<birth_place>", gender "<gender>", name "<name>", phone_number "<phone_number>", ssn "<ssn>", surname "<surname>", username "<username>"
+
+    Examples:
+      | username  | birth_day  | birth_place | gender | name | phone_number | ssn         | surname |
+      | johndoe12 | 2000-01-01 | New York    | 0      | John | 102-654-1987 | 109-35-7816 | Doe     |
